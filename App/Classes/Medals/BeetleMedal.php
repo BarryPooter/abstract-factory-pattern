80354e4ef75cce4c10e48bdfac5bff73835fe6a8
Remy Kooistra <remy.kooistra@keukenmagazijn.nl>
2018-06-25 17:22:35 +0200

    Construct the subclasses of the MetabotMedal superclass

--- App/Classes/Medals/BeetleMedal.php
+++ App/Classes/Medals/BeetleMedal.php
@@ -6,4 +6,12 @@ use App\Classes\Abstracts\MedabotMedal;
 
 class BeetleMedal extends MedabotMedal
 {
+    /**
+     * BeetleMedal constructor.
+     */
+    public function __construct()
+    {
+        $this->setName("Beetle Medal");
+        $this->setSpecialAbility("fire an Ultra Shot, dealing huge amounts of damage.");
+    }
 }
--- App/Classes/Medals/DragonMedal.php
+++ App/Classes/Medals/DragonMedal.php
@@ -6,4 +6,12 @@ use App\Classes\Abstracts\MedabotMedal;
 
 class DragonMedal extends MedabotMedal
 {
+    /**
+     * DragonMedal constructor.
+     */
+    public function __construct()
+    {
+        $this->setName("Dragon Medal");
+        $this->setSpecialAbility("set the opponent on fire, dealing damage every turn which can not be undone.");
+    }
 }
