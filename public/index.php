084a8ba62f3a0657f8432effbe5e54259a50ed38
Remy Kooistra <remy.kooistra@keukenmagazijn.nl>
2018-07-02 17:09:51 +0200

    front-end stuff

--- public/index.php
+++ public/index.php
@@ -3,11 +3,11 @@
 // automatically by the Composer.
 include_once __DIR__ . '/../vendor/autoload.php';
 
-// Medabot
-
 // -------------------------------------------
 //  BLADE TEMPLATE ENGINE IMPLEMENTATION
 // -------------------------------------------
 // Instantiate the Blade module to automatically load the home view.
 $blade = new \Jenssegers\Blade\Blade(__DIR__. '/../resources/views', __DIR__. '/../storage/cache');
-echo $blade->make('home', []);
+echo $blade->make('home', [
+    'rangedMedabot' => (new \App\Factories\RangedMedabotFactory())->build()
+]);
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,25 @@
 @extends('layouts.app')
 
 @section('content')
-    <p>My awesome application</p>
+    <div class="info-block">
+        <span style="font-weight: 600;">I have a ranged Medabot that exists of the following parts:</span>
+        <ul>
+            <li><strong>Head:</strong> {{ $rangedMedabot->getHead()->getName() }} which can deal {{ $rangedMedabot->getHead()->getDamageOutput() }} damage.</li>
+            <li><strong>Left Arm:</strong> {{ $rangedMedabot->getArm('left')->getName() }} which can deal {{ $rangedMedabot->getArm('left')->getDamageOutput() }} damage.</li>
+            <li><strong>Right Arm:</strong> {{ $rangedMedabot->getArm('right')->getName() }} which can deal {{ $rangedMedabot->getArm('right')->getDamageOutput() }} damage.</li>
+            <li><strong>Left Leg:</strong> {{ $rangedMedabot->getLeg('left')->getName() }} which can deal {{ $rangedMedabot->getLeg('left')->getDamageOutput() }} damage.</li>
+            <li><strong>Right Leg:</strong> {{ $rangedMedabot->getLeg('right')->getName() }} which can deal {{ $rangedMedabot->getLeg('right')->getDamageOutput() }} damage.</li>
+        </ul>
+    </div>
+
+    <div class="info-block">
+        <span style="font-weight: 600;">I have a melee Medabot that exists of the following parts:</span>
+        <ul>
+            <li><strong>Head:</strong> {{ $rangedMedabot->getHead()->getName() }} which can deal {{ $rangedMedabot->getHead()->getDamageOutput() }} damage.</li>
+            <li><strong>Left Arm:</strong> {{ $rangedMedabot->getArm('left')->getName() }} which can deal {{ $rangedMedabot->getArm('left')->getDamageOutput() }} damage.</li>
+            <li><strong>Right Arm:</strong> {{ $rangedMedabot->getArm('right')->getName() }} which can deal {{ $rangedMedabot->getArm('right')->getDamageOutput() }} damage.</li>
+            <li><strong>Left Leg:</strong> {{ $rangedMedabot->getLeg('left')->getName() }} which can deal {{ $rangedMedabot->getLeg('left')->getDamageOutput() }} damage.</li>
+            <li><strong>Right Leg:</strong> {{ $rangedMedabot->getLeg('right')->getName() }} which can deal {{ $rangedMedabot->getLeg('right')->getDamageOutput() }} damage.</li>
+        </ul>
+    </div>
 @endsection
