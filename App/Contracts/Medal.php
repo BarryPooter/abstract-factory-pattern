eecd528e1291e9fe132055072e9ccfd1fb71ab5c
Remy Kooistra <remy.kooistra@keukenmagazijn.nl>
2018-07-02 10:55:28 +0200

    Added methods to the Medal contracdt

--- App/Contracts/Medal.php
+++ App/Contracts/Medal.php
@@ -6,4 +6,9 @@ namespace App\Contracts;
 
 interface Medal
 {
+    public function getName() : string;
+    public function getSpecialAbility() : string;
+
+    public function setName(string $name): void;
+    public function setSpecialAbility(string $special): void;
 }
